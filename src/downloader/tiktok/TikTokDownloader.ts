b18b0033f26322f958158c32e5c554c02cd8471b
NikitaShevchuk <shevchuk.nikita.gh@gmail.com>
2023-01-30 00:38:36 +0200

    Add tiktok video download to server

--- src/downloader/tiktok/TikTokDownloader.ts
+++ src/downloader/tiktok/TikTokDownloader.ts
@@ -1,6 +1,12 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import fs from "fs";
+import internal from "stream";
 import DialogWithUser from "../../DialogWithUser";
 import { MessageBodyWithVideo } from "../../messages/Types";
+
+interface FileName {
+    fileName: string;
+}
 
 export class TikTokDownloader {
     private sourceLink: string;
@@ -13,20 +19,40 @@ export class TikTokDownloader {
 
     public async download() {
         try {
-            const data = { url: this.sourceLink };
-            const response = await axios.post(`https://downloader.bot/api/tiktok/info`, data);
-            // const selector = Cheerio.load(response.data);
-            // const videoUrl = selector("a[type=no-watermark]").attr("href");
-            const videoUrl = response.data.data.mp4;
-            console.log(videoUrl);
+            const video = await this.getVideoStream();
+            const { fileName } = await this.pipeVideoStream(video);
+            const videoPath = `${process.env.HOST || "http://localhost:80"}/download/${fileName}`;
             const messageBody: MessageBodyWithVideo = {
-                video: videoUrl || "",
-                caption: videoUrl,
+                video: videoPath || "",
+                caption: videoPath,
             };
-            // if (videoUrl) DialogWithUser.sendVideoToUser(this.chatId, messageBody);
+            if (videoPath) DialogWithUser.sendVideoToUser(this.chatId, messageBody);
         } catch (error: any) {
             DialogWithUser.sendErrorMessageToUser(this.chatId);
-            console.log(error.response.data);
+            console.log(error);
         }
+    }
+
+    private async getVideoStream(): Promise<internal.Readable> {
+        const data = { url: this.sourceLink };
+        const response = await axios.post(`https://downloader.bot/api/tiktok/info`, data);
+        const videoUrl = response.data?.data.mp4;
+        return axios
+            .get<internal.Readable>(videoUrl, { responseType: "stream" })
+            .then((response) => response.data);
+    }
+
+    private async pipeVideoStream(video: internal.Readable): Promise<FileName> {
+        const fileName = new Date().getTime().toString();
+        return new Promise<FileName>((resolve, reject) => {
+            video
+                .pipe(fs.createWriteStream(`downloads/${fileName}.mp4`))
+                .on("finish", () => {
+                    resolve({ fileName });
+                })
+                .on("error", (error) => {
+                    console.log(error);
+                });
+        });
     }
 }
