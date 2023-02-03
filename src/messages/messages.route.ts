a388dead53632560c2805cccfd06ecc4aef766c0
NikitaShevchuk <shevchuk.nikita.gh@gmail.com>
2023-02-03 09:30:14 +0200

    fix static files

--- src/messages/messages.route.ts
+++ src/messages/messages.route.ts
@@ -6,8 +6,45 @@ import messagesController from "./messages.controller";
 const messagesRoute = express.Router();
 
 messagesRoute.post("", messagesController.receiveMessage);
+
 messagesRoute.get("", (request: express.Request, response: express.Response) => {
     response.sendFile(path.join(buildPath, `index.html`));
 });
+
+messagesRoute.get(
+    "/portfolio/:fileName",
+    (request: express.Request, response: express.Response) => {
+        const fileName = request.params.fileName;
+        if (!fileName) return response.status(404).json("not found");
+        response.sendFile(path.join(buildPath, fileName));
+    }
+);
+
+messagesRoute.get(
+    "/portfolio/static/css/:fileName",
+    (request: express.Request, response: express.Response) => {
+        const fileName = request.params.fileName;
+        if (!fileName) return response.status(404).json("not found");
+        response.sendFile(path.join(`${buildPath}/static/css`, fileName));
+    }
+);
+
+messagesRoute.get(
+    "/portfolio/static/js/:fileName",
+    (request: express.Request, response: express.Response) => {
+        const fileName = request.params.fileName;
+        if (!fileName) return response.status(404).json("not found");
+        response.sendFile(path.join(`${buildPath}/static/js`, fileName));
+    }
+);
+
+messagesRoute.get(
+    "/portfolio/static/media/:fileName",
+    (request: express.Request, response: express.Response) => {
+        const fileName = request.params.fileName;
+        if (!fileName) return response.status(404).json("not found");
+        response.sendFile(path.join(`${buildPath}/static/media`, fileName));
+    }
+);
 
 export default messagesRoute;
